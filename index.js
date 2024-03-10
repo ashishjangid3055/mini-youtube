461dc7bf1e5e061072598787fbb168f1f2c1ea28
Ashish Jangid <akjangid3055@gmail.com>
2024-03-10 13:01:56 +0530

    add user and video model

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`))
+})
+.catch(err => console.log('Mongodb connection error', err))
