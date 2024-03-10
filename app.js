9c9ecf508c49c063b82a27faede829ef74c75c70
Ashish Jangid <akjangid3055@gmail.com>
2024-03-10 20:29:32 +0530

    add register controller and bug fixes

--- app.js
+++ app.js
@@ -11,4 +11,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// import routes
+
+import userRouter from "./routes/user.route.js";
+
+// routes declartion
+app.use("/api/v1/users", userRouter);
+
 export { app } 
--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 8000;
 
 connectDB()
 .then(() => {
-    app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`))
+    app.listen(PORT, () => console.log(`⚙️  Server is running on port : ${PORT}`))
 })
 .catch(err => console.log('Mongodb connection error', err))
