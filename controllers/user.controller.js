b47e52ab1720432ca956a91729bf9aaa2a55379a
Ashish Jangid <akjangid3055@gmail.com>
2024-03-10 22:44:19 +0530

    fixed all register controller bugs

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -18,15 +18,19 @@ const registerUser = asyncHandler(async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists");
     }
-    
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+    
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
     }
-
     const avatar = await uploadOnCloudinary(avatarLocalPath);
+    console.log('avatar', avatar)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
     if (!avatar) {
--- utils/cloudnary.js
+++ utils/cloudnary.js
@@ -16,7 +16,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
 
         // file has been uploaded successfull
-        console.log("file is uploaded on cloudnary", response);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
